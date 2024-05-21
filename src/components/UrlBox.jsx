bd991fd44baef969e4f39abb694772614f8078b7
jihhyeong <jinhinjala26@naver.com>
2024-05-21 20:53:45 +0900

    succed

--- src/components/UrlBox.jsx
+++ src/components/UrlBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import UrlButton from "./UrlButton";
 import UrlList from "./UrlList";
 import "./UrlBox.css";
@@ -7,19 +7,62 @@ const UrlBox = () => {
     const [url, setUrl] = useState("");
     const [urlList, setUrlList] = useState([]);
 
+    useEffect(() => {
+        // 전체 링크 불러오기
+        fetch('http://localhost:8000/short-links/all')
+            .then(response => response.json())
+            .then(data => {
+                setUrlList(data);
+            })
+            .catch(error => {
+                console.error('Error fetching the links:', error);
+            });
+    }, []);
+
     const handleAddurl = () => {
-        if (url) {
-            setUrlList([...urlList, url]);
-            setUrl("");
+        let formattedUrl = url;
+
+        // URL에 프로토콜이 없으면 추가
+        if (!/^https?:\/\//i.test(url)) {
+            formattedUrl = 'http://' + url;
         }
-        else {
-            alert("URL을 입력해주세요.");
-        }
+
+        console.log('Adding URL:', formattedUrl); // 디버깅용 로그
+        fetch('http://localhost:8000/short-links', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ originUrl: formattedUrl }),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Response Data:', data);  // 응답 데이터 출력
+                setUrlList([...urlList, data]); // URL 목록 업데이트
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
     };
 
-    const handleRemoveUrl = (index) => {
-        const newUrlList = urlList.filter((_, i) => i !== index);
-        setUrlList(newUrlList);
+    const handleRemoveUrl = (index, id) => {
+        fetch(`http://localhost:8000/short-links/delete/${id}`, {
+            method: 'DELETE',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                setUrlList(urlList.filter((_, i) => i !== index));
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
     };
 
     return (
@@ -36,7 +79,6 @@ const UrlBox = () => {
             </div>
             <UrlList urlList={urlList} onRemoveUrl={handleRemoveUrl} />
         </div>
-
     );
 };
 
--- src/components/UrlList.jsx
+++ src/components/UrlList.jsx
@@ -4,15 +4,16 @@ import "./UrlList.css";
 const UrlList = ({ urlList, onRemoveUrl }) => {
     return (
         <div className="url-list">
-            {urlList.map((url, index) => (
+            {urlList.map((urlData, index) => (
                 <div key={index} className="url-item">
+                    <button className="remove-button" onClick={() => onRemoveUrl(index, urlData.id)}>X</button>
                     <div className="url-content">
-                        <h5>longUrl</h5>
-                        <div>{url}</div>
-                        <h5>shortUrl</h5>
-                        <div>{/* 짧은 URL 로직 추가 */}</div>
+                        <h5>Long URL: </h5>
+                        <div><a href={urlData.originUrl} target="_blank" rel="noopener noreferrer">{urlData.originUrl}</a></div>
+                        <h5>Short URL: </h5>
+                        <div><a href={`http://localhost:8000/short-links/${urlData.hash}`} target="_blank" rel="noopener noreferrer">{`http://localhost:8000/short-links/${urlData.hash}`}</a></div>
+
                     </div>
-                    <button className="remove-button" onClick={() => onRemoveUrl(index)}>X</button>
                 </div>
             ))}
         </div>
