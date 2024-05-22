4b80d64f0d1c6811c3447b5411c862b94ec868c5
jihhyeong <jinhinjala26@naver.com>
2024-05-22 14:47:55 +0900

    copy-button

--- src/components/UrlList.jsx
+++ src/components/UrlList.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./UrlList.css";
 
 const UrlList = ({ urlList, onRemoveUrl }) => {
+    const copyToClipboard = (text) => {
+        navigator.clipboard.writeText(text);
+    };
     return (
         <div className="url-list">
             {urlList.map((urlData, index) => (
@@ -11,7 +14,9 @@ const UrlList = ({ urlList, onRemoveUrl }) => {
                         <h5>Long URL: </h5>
                         <div><a href={urlData.originUrl} target="_blank" rel="noopener noreferrer">{urlData.originUrl}</a></div>
                         <h5>Short URL: </h5>
-                        <div><a href={`http://localhost:8000/short-links/${urlData.hash}`} target="_blank" rel="noopener noreferrer">{`http://localhost:8000/short-links/${urlData.hash}`}</a></div>
+                        <div><a href={`http://localhost:8000/short-links/${urlData.hash}`} target="_blank" rel="noopener noreferrer">{`http://localhost:8000/short-links/${urlData.hash}`}</a>
+                            <button onClick={() => copyToClipboard(`http://localhost:8000/short-links/${urlData.hash}`)}>Copy</button>
+                        </div>
 
                     </div>
                 </div>
